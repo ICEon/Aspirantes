b2f8cb5a226d4ba24932bfe37a39a1379b29c26b
Eon Matrix <matrix_eon@yahoo.com>
2013-06-01 10:21:34 -0500

    Uno

---
+++ js/actions.js
@@ -0,0 +1,96 @@
+function Reacciona(campo) { 
+setTimeout(function(){
+		$('div.reg').hide();
+var b = document.getElementById('campo').value;
+b = b.toUpperCase();
+$('div.reg:contains('+b+')').show();
+		}, 500);
+}
+
+function guardar(Q,P,S,T,C){
+
+	datos = "Quien="+Q+"Primera="+P+"Segunda="+S+"Tercera="+T+"Cuarta="+C;
+	$.ajax({
+		type: "POST",
+		url: "http://cbtis16.net46.net/guardar.php",
+		data: datos
+	}).done(function(msg) {
+
+alert ("Opciones Guardadas");
+
+
+
+
+	});
+}
+
+
+
+function loginConn(reg){
+
+	datos = "registro="+reg;
+	$.ajax({
+		type: "POST",
+		url: "http://cbtis16.net46.net/buscar.php",
+		data: datos
+	}).done(function(msg) {
+
+var Datos = JSON.parse (msg);
+
+
+$('#devic table td').eq(1).text(Datos['Nombre']);
+$('#devic table td').eq(3).text(Datos['ApellidoP']);
+$('#devic table td').eq(5).text(Datos['ApellidoM']);
+$('#devic table td').eq(7).text(Datos['Secundaria']);
+$('#devic table td').eq(9).text(Datos['Secundaria']);
+$('#devic table td').eq(11).text(Datos['POpcion']);
+$('#devic table td').eq(13).text(Datos['SOpcion']);
+$('#devic table td').eq(15).text(Datos['TOpcion']);
+$('#devic table td').eq(17).text(Datos['COpcion']);
+
+	});
+}
+
+
+$(document).ready(function(e) {
+
+
+
+
+    document.addEventListener("deviceready", function(){
+			$('.Send').tap(function(){
+		var formulario = $(this).parents('form');
+
+	switch(formulario.attr('name'))
+	{
+		
+			case 'encontrar':
+			
+				var registro = formulario.children('input:eq(0)').val();
+				loginConn(registro);
+	
+				break;	
+							case 'opciones':
+							
+                var Quien = document.getElementById("Registro").value;
+				var Primera = document.getElementByName("POpcion").value;
+				var Segunda = document.getElementByName("SOpcion").value;
+				var Tercera = document.getElementByName("TOpcion").value;
+				var Cuarta = document.getElementByName("COpcion").value;
+				
+				guardar(Quien, Primera,Segunda,Tercera,Cuarta);
+	
+				break;	
+
+		}
+	});
+
+
+			
+
+
+
+
+		
+	});
+});
