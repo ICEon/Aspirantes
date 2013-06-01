6daf0c24ea7c3a33f0513702a4f639ca0bffc801
ICEon <matrix_eon@yahoo.com>
2013-06-01 11:57:59 -0400

    Update actions.js

--- js/actions.js
+++ js/actions.js
@@ -31,7 +31,7 @@ function loginConn(reg){
 	datos = "registro="+reg;
 	$.ajax({
 		type: "POST",
-		url: "192.168.1.69/buscar.php",
+		url: "http://cbtis16.net46.net/buscar.php",
 		data: datos
 	}).done(function(msg) {
 
